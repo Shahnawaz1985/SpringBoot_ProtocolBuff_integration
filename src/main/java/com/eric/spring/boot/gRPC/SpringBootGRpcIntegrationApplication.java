c936128efcf364f5105586f8801d61c3d0abb92e
Shahnawaz1985 <shahnawaz.akhter@outlook.com>
2021-03-12 14:11:01 +0530

    pom.xml updated for grpc server configuration.

--- src/main/java/com/eric/spring/boot/gRPC/SpringBootGRpcIntegrationApplication.java
+++ src/main/java/com/eric/spring/boot/gRPC/SpringBootGRpcIntegrationApplication.java
@@ -3,6 +3,11 @@ package com.eric.spring.boot.gRPC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author Shahnawaz
+ *
+ */
 @SpringBootApplication
 public class SpringBootGRpcIntegrationApplication {
 
