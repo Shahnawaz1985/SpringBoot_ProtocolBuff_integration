4bc8448c9ac10a5875ee248cc3f94ca00852c7ef
Shahnawaz1985 <shahnawaz.akhter@outlook.com>
2021-03-12 13:27:35 +0530

    Initial checkin for Spring Boot Protocol Buffer integration POC.

---
+++ src/main/java/com/eric/spring/boot/gRPC/SpringBootGRpcIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.eric.spring.boot.gRPC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGRpcIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGRpcIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eric/spring/boot/gRPC/SpringBootGRpcIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eric.spring.boot.gRPC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGRpcIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
